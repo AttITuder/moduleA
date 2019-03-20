eeb499d33cdb2cc72dde8c4bb9af9bb2dd4ed842
dpc <dupengcan@126.com>
2019-03-20 09:12:26 +0800

    提交

---
+++ app/src/main/java/module/git/com/modulea/MainActivity.java
@@ -0,0 +1,13 @@
+package module.git.com.modulea;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
